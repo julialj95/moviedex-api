a3de58cacd69b1885923820ee093d655f48386bd
Julia Johnson <julialj95@gmail.com>
2021-02-14 11:51:49 -0600

    prepared for production

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
 const helmet = require("helmet");
 const cors = require("cors");
 const MOVIEDEX = require("./moviedex.json");
@@ -9,7 +10,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 
-app.use(morgan("dev"));
+app.use(morgan(morganSetting));
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
@@ -43,6 +44,16 @@ app.get("/movie", function handleGetMovies(req, res) {
   }
 
   res.send(movieList);
+});
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
 
 module.exports = app;
